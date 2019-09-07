5ce2716170ec4d3957c2cdbf5a16441cf75ad8cb
sakitam21 <1983409794@qq.com>
2019-09-07 17:37:18 +0800

    layout

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
 import router from './router'
 
 import axios from 'axios'
 axios.defaults.baseURL = process.env.API_ROOT
 
-import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 Vue.use(ElementUI);
 
