133ab8066b850bb03281e801750facd1f1cb1c6c
sakitam21 <1983409794@qq.com>
2019-09-10 13:09:16 +0800

    https

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT:'"https://192.168.43.200:3001/"'
+  API_ROOT:'"https://localhost:3001/"'
 })
